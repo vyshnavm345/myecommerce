d7ce0f62c810585d23a26686b60756f4e9678073
Vishnu M <vyshnav.m345@gmail.com>
2023-11-09 19:03:20 +0530

    sales report update

--- myecommerce/settings.py
+++ myecommerce/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = "django-insecure-y(tm@171dx$=&k+wswr$k#ha3q5#mhj3&=&-m+z1$balin&hz_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
-# ALLOWED_HOSTS = ['51.20.128.236', "0.0.0.0"]
+# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['51.20.128.236', "0.0.0.0"]
 
 
 # Application definition
@@ -44,13 +44,13 @@ INSTALLED_APPS = [
     "referals",
     "orderInvoice",
     "salesReport",
-    # "corsheaders",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    # "corsheaders.middleware.CorsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -151,12 +151,12 @@ EMAIL_HOST_PASSWORD = 'ymdm cgez watr lhil'
 EMAIL_USE_SSL = False
 
 
-# CORS_ALLOWED_ORIGINS = [
-#    "http://51.20.128.236",
-#     "http://51.20.128.236/"
-# ]
+CORS_ALLOWED_ORIGINS = [
+   "http://51.20.128.236",
+    "http://51.20.128.236/"
+]
 
-# CSRF_TRUSTED_ORIGINS = [
-#     "http://51.20.128.236",
-#     "http://51.20.128.236/"
-# ]
+CSRF_TRUSTED_ORIGINS = [
+    "http://51.20.128.236",
+    "http://51.20.128.236/"
+]
